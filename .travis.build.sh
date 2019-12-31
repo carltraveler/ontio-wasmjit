9943eeb428632acd75d276578a14548e43679fbe
steven <chenglin@ont.com>
2019-12-31 14:29:35 +0800

    ci

--- .travis.build.sh
+++ .travis.build.sh
@@ -2,19 +2,26 @@
 set -e
 set -x
 
-rustc --version
-
-if rustup component add clippy;
-then
-	cargo clippy --all --all-targets --all-features -- -D warnings
-else
-	echo 'Skipping clippy';
-fi
-
-cargo fmt --all -- --check
-cargo build --all
-cargo test --all
-
-cd wasmjit-harness
-cargo run -- -q
+[[ $1 == 1 ]] && {
+	cd ..
+	cd wasmjit-go-test/gotest
+	cp ../../target/release/libwasmjit_go_test.a .
+	go test
+} || {
+	rustc --version
+	
+	if rustup component add clippy;
+	then
+		cargo clippy --all --all-targets --all-features -- -D warnings
+	else
+		echo 'Skipping clippy';
+	fi
+	
+	cargo fmt --all -- --check
+	cargo build --all
+	cargo test --all
+	
+	cd wasmjit-harness
+	cargo run -- -q
+}
 
